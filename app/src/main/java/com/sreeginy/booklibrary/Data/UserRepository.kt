60d6677aa04fc5139909a0de1e223d82b136c2ae
Sreeginy <sreeginy98@gmail.com>
2023-08-03 22:10:33 +0530

    create Repo

---
+++ app/src/main/java/com/sreeginy/booklibrary/Data/UserRepository.kt
@@ -0,0 +1,12 @@
+package com.sreeginy.booklibrary.Data
+
+import androidx.lifecycle.LiveData
+
+class UserRepository(private val userDao: UserDao) {
+
+    val readAllData: LiveData<List<User>> = userDao.readAllData()
+
+    suspend fun addUser(user: User) {
+        userDao.addUser(user)
+    }
+}
