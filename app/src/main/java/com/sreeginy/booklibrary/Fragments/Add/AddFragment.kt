57869b4a939689c2712924d41061ee3406a33615
Sreeginy <sreeginy98@gmail.com>
2023-08-04 21:56:34 +0530

    modify fragment

--- app/src/main/java/com/sreeginy/booklibrary/Fragments/Add/AddFragment.kt
+++ app/src/main/java/com/sreeginy/booklibrary/Fragments/Add/AddFragment.kt
@@ -1,10 +1,11 @@
-package com.sreeginy.booklibrary
+package com.sreeginy.booklibrary.Fragments.Add
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.sreeginy.booklibrary.R
 
 class addFragment : Fragment() {
 
--- app/src/main/java/com/sreeginy/booklibrary/Fragments/List/ListFragment.kt
+++ app/src/main/java/com/sreeginy/booklibrary/Fragments/List/ListFragment.kt
@@ -1,10 +1,11 @@
-package com.sreeginy.booklibrary
+package com.sreeginy.booklibrary.Fragments.List
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.sreeginy.booklibrary.R
 
 class ListFragment : Fragment() {
 
@@ -15,5 +16,5 @@ class ListFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_list, container, false)
     }
-    
+
 }
